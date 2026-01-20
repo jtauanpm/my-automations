2d7559b82a45e565e40d93c924b11d94de6b3e4d
Jonathan <jtauan10@gmail.com>
2026-01-20 18:24:27 -0300

    ApplicationService.cs

---
+++ Automations.Instagram/Services/ApplicationService.cs
@@ -0,0 +1,74 @@
+using Automations.Instagram.DTOs;
+using Serilog;
+
+namespace Automations.Instagram.Services;
+
+public static class ApplicationService
+{
+    // a very hopeful attempt to get Instagram to not block me
+    private const int PageSize = 25;
+
+    public static async Task UnfollowNonFavoriteUsersAsync()
+    {
+        while (true)
+        {
+            var followingResponse = await GetFirstFollowingUsers();
+
+            if (followingResponse.Users.Count == 0)
+            {
+                Log.Logger.Information("No users returned. Stopping.");
+                break;
+            }
+            
+            foreach (var (user, index) in followingResponse.Users
+                         .Where(u => !u.IsFavorite)
+                         .Select((user, index) => (user, index)))
+            {
+                try
+                {
+                    await InstagramRequestService.MakeUnfollowRequest(user.Pk);
+                    Log.Logger.Information("{Index} - User unfollowed: {User}", index, user);
+
+                    var timeToAwait = TimeSpan.FromSeconds(Random.Shared.Next(5, 70));
+                    await Task.Delay(timeToAwait);
+                }
+                catch (Exception ex)
+                {
+                    Log.Logger.Error(ex, "Error while trying to unfollow {User}", user);
+                    throw;
+                }
+            }
+
+            if (!followingResponse.HasMore)
+            {
+                Log.Logger.Information("No more following users.");
+                break;
+            }
+
+            var betweenCyclesDelay = Random.Shared.Next(5, 181);
+            Log.Logger.Information("---- Awaiting {Minutes} minutes for fetch next users ----", betweenCyclesDelay);
+            await Task.Delay(TimeSpan.FromMinutes(betweenCyclesDelay));
+        }
+    }
+
+    private static async Task<FollowingResponse> GetFirstFollowingUsers()
+    {
+        try
+        {
+            var followingInput = new FollowingRequestInput
+            {
+                Count = PageSize,
+                MaxId = PageSize // always fetch the first page on purpose
+            };
+
+            Log.Logger.Debug("Fetching first {PageSize} following users", PageSize);
+
+            return await InstagramRequestService.MakeFollowingRequest(followingInput);
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Error(ex, "Error when trying to fetch following users");
+            throw;
+        }
+    }
+}
