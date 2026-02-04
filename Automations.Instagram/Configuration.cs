6bf22dab243ae91336be26836fa7d247cb49441a
jtauanpm <jtauan10@gmail.com>
2026-02-03 21:47:50 -0300

    fill OperationControllerOptions

--- Automations.Instagram/Configuration.cs
+++ Automations.Instagram/Configuration.cs
@@ -1,3 +1,4 @@
+using Automations.Instagram.OperationControl;
 using Microsoft.Extensions.Configuration;
 
 namespace Automations.Instagram;
@@ -5,4 +6,11 @@ namespace Automations.Instagram;
 public static class Configuration
 {
     public static IConfiguration Global { get; set; } = null!;
+
+    public static OperationControllerOptions GetOperationControlOptions()
+    {
+        var result = new OperationControllerOptions();
+        Global.GetSection(OperationControllerOptions.SectionName).Bind(result);
+        return result;
+    }
 }
